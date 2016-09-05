475e1e970c83b4d87248e1be7b2c5ae8c7079349
Alexis Vincent <alexisjohnvincent@gmail.com>
2016-09-05 13:00:35 +0200

    Rather use window.location.origin for hmr socket; added tests

---
+++ test/app/app.js
@@ -0,0 +1,16 @@
+import {build} from 'js-kernel'
+
+import reducer from './reducer.js'
+import routes from './routes.js'
+// import {initialState} from './core/entities/utilities.js'
+// import './app.ncss'
+
+const kernel = build({
+    routes,
+    reducer,
+    hmr: {
+        https: true
+    }
+}, {})
+
+kernel.render()
